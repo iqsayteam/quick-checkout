7b1b27d9e369c98655005e740073295cabd7095a
ru-iqsay <109206080+ru-iqsay@users.noreply.github.com>
2024-12-30 20:07:37 +0530

    working

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('getuniquelink', [CheckoutController::class, 'createUniqueLink'])->name('getuniquelink');
 Route::get('getuniquelink/{user_id}', [CheckoutController::class, 'createUniqueLink'])->name('getuniquelink1');
+Route::get('getuniquelink/{user_id}/{services}', [CheckoutController::class, 'createUniqueLink'])->name('getuniquelink1');
